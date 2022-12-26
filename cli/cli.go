8446d9947878b4ab9a2c9c44afe627513011fee6
Rwitesh Bera <rwiteshbera@gmail.com>
2022-12-26 19:38:58 +0530

    Added blockchain network

--- cli/cli.go
+++ cli/cli.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"flag"
 	"fmt"
+	"github.com/rwiteshbera/Hyperion/network"
 	"log"
 	"os"
 	"runtime"
@@ -18,15 +19,17 @@ func (cli *CLI) validateArgs() {
 	}
 }
 
-func (cli *CLI) create() {
+// Create a new wallet
+func (cli *CLI) createWallet() {
 	wallets, _ := wallet.InitWallets()      // Init saved wallets from file
 	newWallet := wallet.CreateWallet(0)     // Create new wallet
-	address := wallets.AddWallet(newWallet) // add it wallets file
+	address := wallets.AddWallet(newWallet) // add it to wallets
 	wallets.SaveFile()                      // save it
 	fmt.Printf("%s\n", address)
 }
 
-func (cli *CLI) list() {
+// List all the save wallets with balances
+func (cli *CLI) listWallets() {
 	wallets, _ := wallet.InitWallets()
 	addresses := wallets.GetAllAddresses()
 	// wallets.SaveFile()
@@ -35,11 +38,17 @@ func (cli *CLI) list() {
 	}
 }
 
+// Start the blockchain server on specific port
+func (cli *CLI) startBlockchainServer(port *string) {
+	network.Server(port)
+}
+
 func (cli *CLI) RunCLI() {
 	cli.validateArgs()
 
-	CreateWalletCMD := flag.NewFlagSet("--create", flag.ExitOnError)
-	ListAddressesCMD := flag.NewFlagSet("--list", flag.ExitOnError)
+	CreateWalletCMD := flag.NewFlagSet("--create", flag.ExitOnError) // --create
+	ListAddressesCMD := flag.NewFlagSet("--list", flag.ExitOnError)  // --list
+	ServerStartupPort := flag.NewFlagSet("--port", flag.ExitOnError) // --port <MENTION_PORT_NUMBER>
 
 	switch os.Args[1] {
 	case "--create":
@@ -52,15 +61,26 @@ func (cli *CLI) RunCLI() {
 		if err != nil {
 			log.Panic(err)
 		}
+	case "--port":
+		err := ServerStartupPort.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+
 	default:
 		runtime.Goexit()
 	}
 
 	if CreateWalletCMD.Parsed() {
-		cli.create()
+		cli.createWallet()
 	}
 	if ListAddressesCMD.Parsed() {
-		cli.list()
+		cli.listWallets()
+	}
+	if ServerStartupPort.Parsed() {
+		port := ServerStartupPort.String("port", "", "The port to listen on")
+		ServerStartupPort.Parse(os.Args[1:])
+		cli.startBlockchainServer(port)
 	}
 
 }
---
+++ network/network.go
@@ -0,0 +1,39 @@
+package network
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+type BlockchainServer struct {
+	port string
+}
+
+func NewBlockchainServer(port string) *BlockchainServer {
+	return &BlockchainServer{port}
+}
+
+func (chain *BlockchainServer) PORT() string {
+	return chain.port
+}
+
+func HelloWorld(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello World")
+}
+
+func (chain *BlockchainServer) Run() {
+	http.HandleFunc("/", HelloWorld)
+
+	// Start the server
+	log.Println("Starting Blockchain server on 0.0.0.0:" + chain.PORT())
+	if err := http.ListenAndServe("0.0.0.0:"+chain.PORT(), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Server(port *string) {
+	p := *port
+	chain := NewBlockchainServer(p)
+	chain.Run()
+}
