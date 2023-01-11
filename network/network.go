8230d6a03f57ba692469f5b7d92b38eac7c5f069
Rwitesh Bera <rwiteshbera@gmail.com>
2023-01-12 01:26:12 +0530

    Updated console message for server startup

--- network/network.go
+++ network/network.go
@@ -1,17 +1,25 @@
 package network
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/rwiteshbera/Hyperion/blockchain"
 	"github.com/rwiteshbera/Hyperion/wallet"
 	"log"
+	"net"
 	"net/http"
 )
 
 func Server(port *string) {
 	fmt.Println("Initializing Blockchain Server...")
 	fmt.Println("Warning: The server will not retain any information once it is turned off. Any data on blocks will be lost once the server is stopped.")
+	fmt.Printf("\n\n")
+	fmt.Printf("Local: http://localhost:%s\n", *port)
+
+	ip, _ := LocalIP() // retrieve the host machine local IP address
+	fmt.Printf("On Your Network: http://%s:%s\n", ip, *port)
+
 	blockchain.BlockchainInstance = blockchain.InitBlockchain()
 	blockchain.BlockchainInstance.StartMining()
 
@@ -79,4 +87,55 @@ func Server(port *string) {
 		log.Panic(err.Error())
 	}
 
+}
+
+// LocalIP : the function makes use of the Go standard library package "net" to retrieve the host machine local IP address
+func LocalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if isPrivateIP(ip) {
+				return ip, nil
+			}
+		}
+	}
+
+	return nil, errors.New("no IP")
+}
+
+// isPrivateIP : It is used to check the address if it is a private IP or not.
+func isPrivateIP(ip net.IP) bool {
+	var privateIPBlocks []*net.IPNet
+	for _, cidr := range []string{
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+
+	return false
 }
